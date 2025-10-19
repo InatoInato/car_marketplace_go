e09e3ee31095322ee26dd3e96675a6b17727e2ed
InatoInato <diasmuratbekff@gmail.com>
2025-10-19 21:54:38 +0500

    Optimized: used goroutines for async calls

--- internal/service/car_service.go
+++ internal/service/car_service.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/InatoInato/car_marketplace_go.git/internal/model"
@@ -39,27 +40,58 @@ func (s *CarService) Create(car *model.Car) error {
 	if err := s.repo.CreateCar(car); err != nil {
 		return err
 	}
-	s.cache.Del(ctx, "cars:all")
+	
+	// Async cache invalidation
+	// Very important 
+	go func(id uint) {
+		_ = s.cache.Del(ctx, "cars:all")
+		_ = s.cache.Del(ctx, fmt.Sprintf("car:%d", id))
+	}(car.ID)
+
 	return nil
 }
 
 func (s *CarService) GetAll() ([]model.Car, error) {
-	val, err := s.cache.Get(ctx, "cars:all").Result()
-	if err == nil {
-		var cars []model.Car
-		_ = json.Unmarshal([]byte(val), &cars)
-		return cars, nil
+	carsChan := make(chan []model.Car, 2)
+	errChan := make(chan error, 2)
+
+	// Try to get from cache
+	go func(){
+		val, err := s.cache.Get(ctx, "cars:all").Result()
+		if err == nil{
+			var cars []model.Car
+			_ = json.Unmarshal([]byte(val), &cars)
+			carsChan <- cars
+			return
+		}
+		errChan <- err
+		carsChan <- nil
+	}()
+
+	go func(){
+		cars, err := s.repo.GetAllCars()
+		if err != nil {
+			errChan <-  err
+			carsChan <- nil
+			return
+		}
+		carsChan <- cars
+		data, _ := json.Marshal(cars)
+		_ = s.cache.Set(ctx, "cars:all", data, 5*time.Minute)
+	}()
+
+	for i := 0; i < 2; i ++{
+		select{
+		case cars := <- carsChan:
+			if cars != nil{
+				return cars, nil
+			}
+		case err := <- errChan:
+			log.Println("Get all error: ", err)
+		}
 	}
 
-	cars, err := s.repo.GetAllCars()
-	if err != nil {
-		return nil, err
-	}
-
-	data, _ := json.Marshal(cars)
-	s.cache.Set(ctx, "cars:all", data, 5*time.Minute)
-
-	return cars, nil
+	return nil, errors.New("failed to get cars from both cache and database")
 }
 
 func (s *CarService) GetByID(id uint) (*model.Car, error) {
@@ -85,8 +117,11 @@ func (s *CarService) Update(car *model.Car) error {
 	if err := s.repo.UpdateCar(car); err != nil {
 		return err
 	}
-	s.cache.Del(ctx, "cars:all")
-	s.cache.Del(ctx, fmt.Sprintf("car:%d", car.ID))
+
+	go func(id uint){
+		s.cache.Del(ctx, "cars:all")
+		s.cache.Del(ctx, fmt.Sprintf("car:%d", car.ID))
+	}(car.ID)
 	return nil
 }
 
@@ -94,7 +129,10 @@ func (s *CarService) Delete(id uint) error {
 	if err := s.repo.DeleteCar(id); err != nil {
 		return err
 	}
-	s.cache.Del(ctx, "cars:all")
-	s.cache.Del(ctx, fmt.Sprintf("car:%d", id))
+
+	go func(id uint){
+		s.cache.Del(ctx, "cars:all")
+		s.cache.Del(ctx, fmt.Sprintf("car:%d", id))
+	}(id)
 	return nil
 }
