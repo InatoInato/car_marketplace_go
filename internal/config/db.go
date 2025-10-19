960878b08946f1387aa6146994de3f22f607cdcd
InatoInato <diasmuratbekff@gmail.com>
2025-10-19 20:09:36 +0500

    Fixed: Connection to db with retrying

--- internal/config/db.go
+++ internal/config/db.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/InatoInato/car_marketplace_go.git/internal/model"
 	"gorm.io/driver/postgres"
@@ -12,19 +13,34 @@ import (
 
 var DB *gorm.DB
 
-func ConnectDB(){
+func ConnectDB() {
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC", 
-	host, user, password, dbname, port)
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
+		host, user, password, dbname, port,
+	)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil{
-		log.Fatal("Failed to connect to database!")
+	var db *gorm.DB
+	var err error
+
+
+	maxAttempts := 10
+	for i := 1; i <= maxAttempts; i++ {
+		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err == nil {
+			break
+		}
+		log.Printf("Failed to connect to database, attempt %d/%d. Retrying in 2s...", i, maxAttempts)
+		time.Sleep(2 * time.Second)
+	}
+
+	if err != nil {
+		log.Fatal("Failed to connect to database after multiple attempts:", err)
 	}
 
 	log.Println("DB Connected")
@@ -33,8 +49,8 @@ func ConnectDB(){
 		&model.User{},
 		&model.Car{},
 	)
-	if err != nil{
-		log.Fatal("Failed to migrate database!")
+	if err != nil {
+		log.Fatal("Failed to migrate database:", err)
 	}
 
 	DB = db
